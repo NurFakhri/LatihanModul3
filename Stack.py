9e31f90881ee081cdac6293178c3746e23a2e34f
Muhammad Hadi Nur Fakhri <91940589+NurFakhri@users.noreply.github.com>
2023-04-06 15:51:18 +0700

    Create Stack.py

---
+++ Stack.py
@@ -0,0 +1,13 @@
+stack = []
+
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print('\nElements popped from stack)
+      print(stack.pop())
+      print(stack.pop())
+      print(stack.pop())
+
+print('\nStack after elements are popped: ')
+print(stack)
